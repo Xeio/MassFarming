bf387230e9a1f698b36cb72dffd90921219b61a0
Joshua Shaffer <xeio@live.com>
2022-12-06 20:38:36 -0500

    Bump version to v1.5

--- MassFarming/MassFarming.cs
+++ MassFarming/MassFarming.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace MassFarming
 {
-    [BepInPlugin("xeio.MassFarming", "MassFarming", "1.4")]
+    [BepInPlugin("xeio.MassFarming", "MassFarming", "1.5")]
     public class MassFarming : BaseUnityPlugin
     {
         public static ConfigEntry<KeyboardShortcut> MassActionHotkey { get; private set; }
