bc2fe4583fc30d120e4e66c6a4fd6276cbb19d65
perdjesk <Perdjesk@users.noreply.github.com>
2023-08-24 19:07:30 +0200

    Target .net v4.7.2 for Array.Emtpy. Adapt for Valheim 0.217.14

--- MassFarming/MassFarming.cs
+++ MassFarming/MassFarming.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace MassFarming
 {
-    [BepInPlugin("xeio.MassFarming", "MassFarming", "1.7")]
+    [BepInPlugin("xeio.MassFarming", "MassFarming", "1.8")]
     public class MassFarming : BaseUnityPlugin
     {
         public static ConfigEntry<KeyboardShortcut> MassActionHotkey { get; private set; }
